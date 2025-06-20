9bf9a3779a1cb9dfc261b8beca90135238d50b30
cenrique88 <cenrique88.uy@gmail.com>
2025-06-19 21:14:34 -0300

    add mas datos a los esquemas

--- models/Clientes.js
+++ models/Clientes.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 
 const clientesSchema = new mongoose.Schema({
   nombre_cliente: { type: String, required: true },
+  nombre_juridico: { type: String, required: true },
+  direccion: { type: String, required: true },
   email: { type: String, required: true },  
 });
 
