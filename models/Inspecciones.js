99b00ce12d39c915472181725adb871f119c6d29
Carlos Enrique <126535823+cenrique88@users.noreply.github.com>
2025-06-20 22:52:37 -0300

    cambio de datos en inspecciones

--- models/Inspecciones.js
+++ models/Inspecciones.js
@@ -5,7 +5,7 @@ const inspeccionesSchema = new mongoose.Schema({
     fecha_inspeccion: { type: Date, required: true },
     id_extintor: { type: String, required: true },
     cliente: {type: String, required: true},
-    estado_anterior: Schema.Types.Mixed,
+   // estado_anterior: Schema.Types.Mixed,
 
 });
 
