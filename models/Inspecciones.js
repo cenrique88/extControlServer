e75e3a3d05119b68d6d59cd27658c14ea97f8946
Carlos Enrique <126535823+cenrique88@users.noreply.github.com>
2025-06-20 23:02:49 -0300

    1

--- models/Inspecciones.js
+++ models/Inspecciones.js
@@ -5,7 +5,7 @@ const inspeccionesSchema = new mongoose.Schema({
     fecha_inspeccion: { type: Date, required: true },
     id_extintor: { type: String, required: true },
     cliente: {type: String, required: true},
-    estado_anterior: {type: Schema.Types.Mixed, default: {}, required: true},
+    estado_anterior: {type: Schema.Types.Mixed, required: true},
 
 });
 
