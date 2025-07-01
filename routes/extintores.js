82cb9b8c54d9996ae9d0bfaecfb27a42cf83778e
Carlos Enrique <126535823+cenrique88@users.noreply.github.com>
2025-07-01 00:18:00 -0300

    uri

--- routes/extintores.js
+++ routes/extintores.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 
-const Extintor = require('./models/Extintor');
+const Extintor = require('../models/Extintor');
 
 
 // MANEJO DEL GET DE LOS TODOS LOS EXTINTORES DE LA BASE DE DATOS:
