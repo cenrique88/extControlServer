3c8b4a3ffd06aaa896b8e2585d7f845e19ef2258
cenrique88.uy <cenrique88.uy@gmail.com>
2025-07-18 01:14:17 +0000

    cambio de elementos en el esquema de extintores

--- models/Extintor.js
+++ models/Extintor.js
@@ -7,14 +7,12 @@ const extintorSchema = new mongoose.Schema({
   cliente: {type: String, required: true},
   ubicacion: { type: String, required: true },
   tipo_extintor: { type: String, required: true },
-  clasificacion: { type: String, required: true },
   capacidad: { type: String, required: true },
   recarga_cada: { type: Number, required: true},
   estado_extintor: { type: String, required: true },
   senalizacion: { type: String, required: true },
   soporte_nicho: { type: String, required: true },
   fecha_recarga: { type: Date, required: true },
-  inspecciones_realizadas: {type: Schema.Types.Mixed, default: {}},
   observaciones: { type: String },
 });
 
