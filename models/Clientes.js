b2f167360b512a9e72de64d51df762408a80fcc1
cenrique88.uy <cenrique88.uy@gmail.com>
2025-07-24 03:19:56 +0000

    resolve error

--- models/Clientes.js
+++ models/Clientes.js
@@ -5,7 +5,7 @@ const clientesSchema = new mongoose.Schema({
   nombre_cliente: { type: String, required: true },
   nombre_juridico: { type: String },
   direccion: { type: String },
-  email: { type: Stringe },  
+  email: { type: String },  
   telefono: { type: Number },
   telefono1: { type: Number },
   telefono2: { type: Number },
